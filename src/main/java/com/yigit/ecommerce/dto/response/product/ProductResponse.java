d3a9d40ec4ceb3d898d7533d42917b07f1ed952b
Yiğit Gülbeyaz <yigitgulbeyaz@outlook.com>
2025-12-18 15:04:12 +0300

    feat: add product DTOs

---
+++ src/main/java/com/yigit/ecommerce/dto/response/product/ProductResponse.java
@@ -0,0 +1,60 @@
+package com.yigit.ecommerce.dto.response.product;
+
+import java.math.BigDecimal;
+
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Long categoryId;
+    private String categoryName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
